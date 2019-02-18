7d9fbb8a77105fbebd143af2c616c194a45d1c7e
inayatkh <inayatkh@gmail.com>
2019-02-18 14:45:42 +0500

    Initial commit

---
+++ tracker/centroidTracking.py
@@ -0,0 +1,9 @@
+'''
+
+    Here, I implement a centroid tracking algorithm
+
+    The primary assumption in this algorithm is that pair of centroids with minimum Euclidean
+    distance between them must be the same object ID.
+
+
+'''
